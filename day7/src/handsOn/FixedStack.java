d5afc33ed068a25d42e3726d9f6cd40cfbca3bce
dac <dac@students.acts.in>
2023-10-13 18:37:39 +0530

    day7_8 assignment commit

--- day7/src/handsOn/FixedStack.java
+++ day7/src/handsOn/FixedStack.java
@@ -1,19 +1,42 @@
 package handsOn;
 
+import java.util.Scanner;
+
 public class FixedStack implements Stack {
-	private int size;
+//	private int length;
+	private int top = -1;
+	public Customer c;
+	Customer[] fStack;
 
 	public FixedStack(int size) {
-		Customer[] fixedCustomer = new Customer[size];
-	}
-
-	@Override
-	public void pop() {
-
+		fStack = new Customer[size];
 	}
 
 	@Override
 	public void push() {
+		if (top == fStack.length - 1)
+			System.out.println("Stack OverFlow!");
+		else {
+			fStack[++top] = customerEntry();
+			System.out.println("Current Position:" + (top + 1));
+		}
+	}
+
+	@Override
+	public void pop() {
+		if (top == -1)
+			System.out.println("Stack UnderFlow!");
+		else {
+			System.out.println("Current Position: " + (top + 1));
+			System.out.println(fStack[top--]);
+		}
 
 	}
+	public Customer customerEntry() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Customer Details (id,anem,address):");
+		Customer c = new Customer(sc.nextInt(), sc.next(), sc.next());
+		return c;
+	}
+
 }
--- day7/src/handsOn/Growable.java
+++ day7/src/handsOn/Growable.java
@@ -1,21 +1,56 @@
 package handsOn;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Growable implements Stack {
+	private int tempsize = 2;
+	private int top = -1;
+	public Customer c;
+	Customer[] gStack;
 
 	public Growable() {
-		List<Customer> growable = new ArrayList<Customer>();
-	}
-
-	@Override
-	public void pop() {
+		gStack = new Customer[1];
 
 	}
 
 	@Override
 	public void push() {
+		if (top == gStack.length - 1) {
+			growStack();
+			gStack[++top] = customerEntry();
+			System.out.println("Current Position:" + (top + 1));
 
+		} else {
+			gStack[++top] = customerEntry();
+			System.out.println("Current Position:" + (top + 1));
+		}
+
+	}
+
+	@Override
+	public void pop() {
+		if (top == -1)
+			System.out.println("Stack UnderFlow!");
+		else {
+			System.out.println("Current Position: " + (top + 1));
+			System.out.println(gStack[top--]);
+		}
+
+	}
+
+	public void growStack() {
+		Customer[] stack2 = new Customer[tempsize * 2];
+		for (int i = 0; i < gStack.length; i++) {
+			stack2[i] = gStack[i];
+		}
+		tempsize=tempsize*2;
+		System.out.println(tempsize);
+		gStack = stack2;
+	}
+	public Customer customerEntry() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Customer Details (id,anem,address):");
+		Customer c = new Customer(sc.nextInt(), sc.next(), sc.next());
+		return c;
 	}
 }
